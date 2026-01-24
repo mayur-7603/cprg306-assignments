5a13e26f5045bb2fff436316563eb96755ee73e2
mayur-7603 <mayurbrahmbhatt763@gmail.com>
2026-01-23 19:54:39 -0700

    created and updated repo

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function Page() {
+  return (
+    <div>
+      <p>Name: Mayur</p>
+      <p>
+        <Link href="https://github.com/mayur-7603/cprg306-assignments.git">
+        My GitHub Repository
+        </Link>
+      </p>
+    </div>
+  );
+}
---
+++ app/week-2/page.js
@@ -0,0 +1,9 @@
+import StudentInfo from './StudentInfo';
+export default function Page() {
+  return (
+    <div>
+      <h1>Shopping List 1</h1>
+      <StudentInfo />
+    </div>
+  );
+}
